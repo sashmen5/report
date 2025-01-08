94aba3c80d91d0b0899515bdfc701c3d110ca1c6
alexandrm <alexandrm@opti.io>
2025-01-08 12:03:20 +0200

    improve gitignore

--- app/router.tsx
+++ app/router.tsx
@@ -1,13 +1,11 @@
 // app/router.tsx
-import { createRouter as createTanStackRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import {createRouter as createTanStackRouter} from '@tanstack/react-router'
+import {routeTree} from './routeTree.gen'
 
 export function createRouter() {
-    const router = createTanStackRouter({
+    return createTanStackRouter({
         routeTree,
     })
-
-    return router
 }
 
 declare module '@tanstack/react-router' {
