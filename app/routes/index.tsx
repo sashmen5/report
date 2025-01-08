c55d6a0f82bc82a1806f652b56ae0840c8e8a6a0
alexandrm <alexandrm@opti.io>
2025-01-08 12:18:59 +0200

    Change from file

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,23 +5,38 @@ import { createServerFn } from '@tanstack/start'
 
 const filePath = 'count.txt'
 
-async function readCount() {
-    return parseInt(
-        await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
-    )
+let counter: number = 10;
+
+async function readCount(): Promise<number> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(counter)
+        }, 20)
+    })
+}
+
+async function writeCount(number): Promise<void> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            counter = number;
+            resolve();
+        }, 20)
+    })
 }
 
 const getCount = createServerFn({
     method: 'GET',
-}).handler(() => {
-    return readCount()
+}).handler(async () => {
+    return await readCount()
 })
 
 const updateCount = createServerFn({ method: 'POST' })
     .validator((d: number) => d)
     .handler(async ({ data }) => {
-        const count = await readCount()
-        await fs.promises.writeFile(filePath, `${count + data}`)
+        const count = await readCount();
+        console.log({ count, data })
+        await writeCount(count + data);
+        console.log('[AFTER]')
     })
 
 export const Route = createFileRoute('/')({
