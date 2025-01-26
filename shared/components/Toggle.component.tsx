42e4149201a4bd6f73fc7630f8c55be5832d6790
alexandrm <alexandrm@opti.io>
2025-01-26 11:46:15 +0200

    fix hover

--- shared/components/Toggle.component.tsx
+++ shared/components/Toggle.component.tsx
@@ -6,12 +6,15 @@ import { type VariantProps, cva } from 'class-variance-authority';
 import { cn } from './cn';
 
 const toggleVariants = cva(
-  'inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium ring-offset-background transition-colors hover:bg-muted hover:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-accent data-[state=on]:text-accent-foreground [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  cn(
+    'inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium ring-offset-background transition-colors hover:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+    'data-[state=on]:bg-accent data-[state=on]:text-accent-foreground',
+  ),
   {
     variants: {
       variant: {
         default: 'bg-transparent',
-        outline: 'border border-input bg-transparent hover:bg-accent hover:text-accent-foreground',
+        outline: 'toggle-hover border border-input bg-transparent hover:text-accent-foreground',
       },
       size: {
         default: 'h-10 min-w-10 px-3',
