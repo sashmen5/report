665727e83ad64d2dac3abca9602b2ab32ed62974
alexandrm <alexandrm@opti.io>
2025-02-13 11:22:04 +0200

    ssr false

--- app/routes/_authed/movies.route.tsx
+++ app/routes/_authed/movies.route.tsx
@@ -5,6 +5,7 @@ import { getMovies } from '../../entities/movie';
 import { MoviesPage } from '../../pages/movies';
 
 export const Route = createFileRoute('/_authed/movies')({
+  ssr: false,
   loader: async () => {
     //rewrite with promise.all
     const [movies, collection] = await Promise.all([getMovies(), getCollection()]);
