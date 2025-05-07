693f1c102d283def7cf86bbb988cb9add81922ec
alexandrm <alexandrm@opti.io>
2025-05-07 09:47:13 +0300

    Update movies.route.tsx

--- app/routes/_authed/movies.route.tsx
+++ app/routes/_authed/movies.route.tsx
@@ -17,6 +17,7 @@ function validateStatus(status?: unknown) {
 }
 
 export const Route = createFileRoute('/_authed/movies')({
+  pendingMinMs: 0,
   pendingComponent: () => <div className={'text-lg font-bold'}>Loading...</div>,
   loaderDeps: ({ search: { status } }) => ({ status }),
   loader: async ({ deps: { status } }) => {
